a0d94520733b07a49bb3f959bf8d5ccb14e9a288
Bruno Junqueira <bruno@telefonicabeta.com>
2014-10-29 16:56:03 -0200

    "Rename Quadra to Places, and adding choice of Content"

--- code.py
+++ code.py
@@ -1,12 +1,19 @@
 import pymongo
 from model import Pessoa
+from model import Places
 
 conn = pymongo.MongoClient("localhost, 27017")
 db = conn.futeba
-
 users = Pessoa(db)
+place = Places(db)
 
-nome = raw_input("Qual o seu nome?\n")
-address = raw_input("Qual o seu endereco?\n")
 
-users.insert_user(nome, address)
+choice = raw_input("Quem e voce?\n1 - Usuario\n2 - Estabelecimento\n")
+if int(choice) == 1:
+	name = raw_input("Qual o seu nome?\n")
+	address = raw_input("Qual o seu endereco?\n")
+	users.insert_user(name, address)
+elif int(choice) == 2:
+	name = raw_input("Qual o nome do seu estabelecimento?\n")
+	address = raw_input("Qual o endereco?\n")
+	place.insert_place(name, address)
