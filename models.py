f3e01a1abee9f68230178fd93995d071e3d39ad7
Bruno Junqueira <bruno@telefonicabeta.com>
2015-02-07 00:59:45 -0200

    "Adding model for databse integration"

--- models.py
+++ models.py
@@ -1,21 +1,48 @@
+import datetime
+import requests
 from mongoengine import *
+
+connect('futeba')
 
 class Pessoa(Document):
     name = StringField()
     bday = DateTimeField()
     genre = StringField()
-    address = TextField()
+    address = StringField()
     city = StringField()
     state = StringField()
-    loc = DictField()
+    loc = GeoPointField()
     email = EmailField()
     password = StringField()
 
+    def parse_date(self, date):
+        d, m, y = date.split("/")
+        bday = datetime.date(int(y), int(m), int(d))
+        return bday
+
+    def get_loc(self, address):
+        add = address.replace(" ", "+")
+        API_KEY = "AIzaSyBk_IAok7qdMik0U8iTyZ3h09xKJ9kBtNs"
+        URL="https://maps.googleapis.com/maps/api/geocode/json?address=" + add + "&key=" + API_KEY
+        req = requests.get(URL)
+        resp = req.json()
+        loc = resp['results'][0]['geometry']['location']
+        return [loc['lat'], loc['lng']]
+
 class Place(Document):
     name = StringField()
-    address = TextField()
+    address = StringField()
     city = StringField()
     state = StringField()
     loc = DictField()
     sport = StringField()
-    obs = TextField()
+    obs = StringField()
+
+    def get_loc(self, address):
+        add = address.replace(" ", "+")
+        API_KEY = "AIzaSyBk_IAok7qdMik0U8iTyZ3h09xKJ9kBtNs"
+        URL="https://maps.googleapis.com/maps/api/geocode/json?address=" + add + "&key=" + API_KEY
+        req = requests.get(URL)
+        resp = req.json()
+        loc = resp['results'][0]['geometry']['location']
+        return [loc['lat'], loc['lng']]
